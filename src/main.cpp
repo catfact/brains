a77fec9164cac13d0e2a42bb2e49b9982ac9b751
emb <emb@catfact.net>
2016-09-27 21:19:04 -0700

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 #define BTSERIAL Serial1
 
-static const int PERIOD_MS = 200;
+static const int PERIOD_MS = 20;
 
 // raw data from ADC
 static uint32_t adcData[NUM_CHANNELS];
@@ -18,14 +18,26 @@ static void tx_adc(void);
 static uint32_t dacValue = 0;
 static uint32_t dacValue_inc = 32;
 
-static const uint32_t DAC_VALUE_MAX = (((1 << 12) >> 2));
+static uint32_t dacValueMax;
+
 static uint32_t update_count = 0;
 
-bool useInternalAdc = true;
+bool useInternalAdc = false;
+
+void setUseInternalAdc(bool val) {
+  useInternalAdc = val;
+  if(val) {
+    dacValueMax = (1 << 12) - 1;
+  } else {
+    dacValueMax  = (((1 << 12) >> 2));
+  }
+}
 
 //extern "C"
 int main(void)
-{ 
+{
+  setUseInternalAdc(false);
+  
   adc_init();
 
   BTSERIAL.begin(115200);
@@ -48,24 +60,29 @@ int main(void)
       update_count = 0;
       // write test data to DAC
       dacValue += dacValue_inc;
-      if(dacValue > DAC_VALUE_MAX) { dacValue = 0; }
+      if(dacValue > dacValueMax) { dacValue = 0; }
       analogWriteDAC0( dacValue + 0x10);
     }
     
     // write data to serial
     tx_adc();
 
-    // 
-    
+    // // check usb serial input
+    while(usb_serial_available()) {
+      int b = usb_serial_getchar();
+      setUseInternalAdc((b == 0x01));
+    }    
   }
 }
 
 // transmit adc data
 static char tx_adc_buf[1024];
-
+//static int count=0;
 void tx_adc() {
-  sprintf(tx_adc_buf, "%08x\r\n", adcData[0] );
-
+  sprintf(tx_adc_buf, "%d\r\n", adcData[0] );
+  // test
+  //sprintf(tx_adc_buf, "%d\r\n", count++ );
+  
   // send same output to usb serial...
   usb_serial_write((const void*)tx_adc_buf, strlen(tx_adc_buf));
   usb_serial_flush_output();
